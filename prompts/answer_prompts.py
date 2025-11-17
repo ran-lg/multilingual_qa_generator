f3c6e8f4a589bc85c4cf8ae750f85da21f5329e6
Valéry Trân Thiên <tranthien.valery@gmail.com>
2025-11-17 15:27:17 +0100

    Format changes

--- prompts/answer_prompts.py
+++ prompts/answer_prompts.py
@@ -3,23 +3,10 @@ answer_prompt1_de = 'Der folgende Text ist ein Auszug aus einem Text.\n\n[Text]\
 answer_prompt1_pl = 'Poniższy tekst jest fragmentem tekstu.\n\n[Tekst]\n\n'
 answer_prompt1_vn = 'Dưới đây là một đoạn trích từ một văn bản.\n\n[Đoạn trích]\n\n'
 
-
-answer_prompt2_en = '''[Instruction]
-
-Answer this question based on the text:'''
-
-answer_prompt2_de = '''[Anweisung]
-
-Beantworten Sie diese Frage anhand des Textes:'''
-
-answer_prompt2_pl = '''[Instrukcja]
-
-Odpowiedz na poniższe pytanie na podstawie tekstu:'''
-
-answer_prompt2_vn = '''[Hướng dẫn]
-
-Trả lời câu hỏi này dựa trên nội dung văn bản:'''
-
+answer_prompt2_en = '''[Instruction]\n\nAnswer this question based on the text:'''
+answer_prompt2_de = '''[Anweisung]\n\nBeantworten Sie diese Frage anhand des Textes:'''
+answer_prompt2_pl = '''[Instrukcja]\n\nOdpowiedz na poniższe pytanie na podstawie tekstu:'''
+answer_prompt2_vn = '''[Hướng dẫn]\n\nTrả lời câu hỏi này dựa trên nội dung văn bản:'''
 
 answer_prompt3_en = 'If the question requires to name a concept based on its definition, quote the text sentence mentioning the concept.'
 answer_prompt3_de = 'Wenn die Frage verlangt, einen Begriff anhand seiner Definition zu benennen, zitieren Sie den Satz aus dem Text, in dem der Begriff erwähnt wird.'
--- prompts/input_prompts.py
+++ prompts/input_prompts.py
@@ -3,11 +3,7 @@ input_prompt1_de = 'Der folgende Text ist ein Auszug aus einem Text.'
 input_prompt1_pl = 'Poniższy tekst jest fragmentem tekstu.'
 input_prompt1_vn = 'Dưới đây là một đoạn trích từ một văn bản.'
 
-
 input_prompt2_en = '''The result you generate should only be a question, which should be as short as possible. What's more, the generated question should not be one that can be answered with a simple yes or no.\n\n[Text]\n\n'''
-
 input_prompt2_de = '''Das Ergebnis sollte nur eine Frage sein, die so kurz wie möglich sein sollte. Außerdem sollte die generierte Frage nicht mit einem einfachen Ja oder Nein beantwortet werden können. \n\n[Text]\n\n'''
-
 input_prompt2_pl = '''Wynik, który generujesz, powinien być tylko pytaniem, które powinno być jak najkrótsze. Co więcej, wygenerowane pytanie nie powinno być pytaniem, na które można odpowiedzieć prostym „tak” lub „nie”.\n\n[Tekst]\n\n'''
-
 input_prompt2_vn = '''Kết quả bạn tạo ra chỉ nên là một câu hỏi, và câu hỏi đó nên ngắn gọn nhất có thể. Hơn nữa, câu hỏi được tạo ra không nên là loại câu hỏi có thể trả lời bằng một câu "có" hoặc "không" đơn giản.\n\n[Đoạn trích]\n\n'''
